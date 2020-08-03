97937a973d94d53df4d7ae0fd20ca7e0e77e4ce6
Adrian Li <li.adrianmc@gmail.com>
2020-08-03 03:49:44 -0400

    Update to Docusaurus version alpha.61 (#32)
    * update docusaurus to alpha 61
    
    * change navbar.links to navbar.items
    
    * add missing picture
    
    * use new theme config API for dark mode toggle icons
    
    * tweak styles for sun/moon emojis positions
    
    * remove example docs
    
    * fix broken link
    
    * make in-line code appear better in alert admonition

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,22 @@ module.exports = {
       theme: require("prism-react-renderer/themes/nightOwl"),
     },
     algolia: {
-      apiKey: 'b3a7b837d4e8ce1e75898ddd39885c19',
-      indexName: 'umaproject',
+      apiKey: "b3a7b837d4e8ce1e75898ddd39885c19",
+      indexName: "umaproject",
+    },
+    colorMode: {
+      switchConfig: {
+        darkIcon: "🌙",
+        darkIconStyle: {
+          marginLeft: "2px",
+          marginTop: "1px",
+        },
+        lightIcon: "☀️",
+        lightIconStyle: {
+          marginLeft: "3px",
+          marginTop: "0.5px",
+        },
+      },
     },
   },
   stylesheets: ["https://use.typekit.net/jll8euv.css"],
