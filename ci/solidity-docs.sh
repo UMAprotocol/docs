f2cb5c81e10a790e9cb58e4dcf80fd0ac3c8eb52
uma-mining <contato@evaldofelipe.com>
2021-07-21 10:29:28 -0300

    Add rsync
    Signed-off-by: uma-mining <contato@evaldofelipe.com>

--- ci/solidity-docs.sh
+++ ci/solidity-docs.sh
@@ -8,11 +8,11 @@ uname -a
 npm -v
 node -v
 
-apk add --update rsync
 npm install solidity-docgen
+npm install rsync
 
 git clone http://github.com/umaprotocol/protocol/
 cd protocol
 yarn
 yarn qbuild
-solidity-docgen --solc-module solc-0.8 -i packages/core/contracts/ -o ./temp-docs/
+solidity-docgen --solc-module solc-0.8 -i packages/core/contracts/ -o ../temp-docs/
