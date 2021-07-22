390da2a87d202ddd2a64f255500774fb59308c86
uma-mining <contato@evaldofelipe.com>
2021-07-22 18:07:54 -0300

    Change exec path
    Signed-off-by: uma-mining <contato@evaldofelipe.com>

--- ci/solidity-docs.sh
+++ ci/solidity-docs.sh
@@ -5,20 +5,17 @@ set -o nounset
 
 echo "installing required tools..."
 yarn install
+npm install solidity-docgen
 
 echo "installing protocol repo..."
 git clone http://github.com/umaprotocol/protocol/
-
 yarn --cwd protocol/
 
 echo "building contracts..."
 yarn --cwd protocol/packages/core/ hardhat compile
 
 echo "generating docs files..."
-cd protocol/
-npm install solidity-docgen
-solidity-docgen --solc-module solc-0.8 -i packages/core/contracts/ -o temp-docs/
+solidity-docgen --solc-module solc-0.8 -i protocol/packages/core/contracts/ -o temp-docs/
 
 echo "configuring docs..."
-cd ..
 mv protocol/temp-docs/ ./docs/contracts
