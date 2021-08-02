78a88bfae479d6bc1b4e3d0039dd77741f474d05
uma-mining <contato@evaldofelipe.com>
2021-08-02 11:36:23 -0300

    Change docgen path
    Signed-off-by: uma-mining <contato@evaldofelipe.com>

--- ci/solidity-docs-test.sh
+++ ci/solidity-docs-test.sh
@@ -5,7 +5,7 @@ set -o nounset
 
 echo "installing required tools..."
 yarn install
-# npm install -g solidity-docgen
+npm install -g solidity-docgen
 
 # echo "installing protocol repo..."
 git clone http://github.com/umaprotocol/protocol/ --single-branch || echo "Repo already exists"
@@ -16,8 +16,8 @@ yarn install --cwd protocol/packages/core --ignore-scripts
 
 echo "generating docs files..."
 rm -rf docs/contracts protocol/temp-docs
-SOL_DOCGEN_PATH=$(yarn bin solidity-docgen)
-cd protocol/ && $SOL_DOCGEN_PATH --solc-module solc-0.8 -i packages/core/contracts/ -t ../ci/ -o temp-docs/
+
+cd protocol/ && solidity-docgen --solc-module solc-0.8 -i packages/core/contracts/ -t ../ci/ -o temp-docs/
 
 echo "configuring docs..."
 mv temp-docs/ ../docs/contracts
