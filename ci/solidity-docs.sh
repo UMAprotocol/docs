5eab9f437fb23c8badf335d6dad1c2cb06fab4f7
evaldofelipe <contato@evaldofelipe.com>
2021-08-31 18:41:58 -0300

    Fix path
    Signed-off-by: evaldofelipe <contato@evaldofelipe.com>

--- ci/solidity-docs.sh
+++ ci/solidity-docs.sh
@@ -16,15 +16,15 @@ yarn install --cwd protocol/packages/core --ignore-scripts
 echo "generating docs files..."
 cd protocol/
 
-docs_ovm_array=($(grep -r -l -i --include="*.sol" "solidity >=0.7.6" ./packages/core/contracts-ovm))
+docs_ovm_array=($(grep -r -l -i --include="*.sol" "solidity >=0.7.6" packages/core/contracts-ovm))
 mkdir ovm-docs/
 for doc in "${docs_ovm_array[@]}"
   do
-    cp $doc ovm-docs/$doc
+    cp $doc ./ovm-docs/$doc
   done
 
 solidity-docgen --solc-module solc-0.8 -i packages/core/contracts/ -t ../ci/ -o temp-docs/
-solidity-docgen --solc-module solc-0.7 -i ovm/ -t ../ci/ -o temp-docs/
+solidity-docgen --solc-module solc-0.7 -i ovm-docs/ -t ../ci/ -o temp-docs/
 
 echo "configuring docs..."
 mv temp-docs/ ../docs/contracts
