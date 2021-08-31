dbc31fd63dfb695be1932f30be398f2c88a56edb
evaldofelipe <contato@evaldofelipe.com>
2021-08-31 19:29:25 -0300

    Test mapfile
    Signed-off-by: evaldofelipe <contato@evaldofelipe.com>

--- ci/solidity-docs.sh
+++ ci/solidity-docs.sh
@@ -4,27 +4,34 @@ set -o errexit
 set -o nounset
 
 
-
-echo "installing required tools..."
-yarn install
-npm install -g solidity-docgen
-
-echo "installing protocol repo..."
-git clone http://github.com/umaprotocol/protocol/ --single-branch || echo "Repo already exists"
-yarn install --cwd protocol/packages/core --ignore-scripts
+#
+# echo "installing required tools..."
+# yarn install
+# npm install -g solidity-docgen
+#
+# echo "installing protocol repo..."
+# git clone http://github.com/umaprotocol/protocol/ --single-branch || echo "Repo already exists"
+# yarn install --cwd protocol/packages/core --ignore-scripts
 
 echo "generating docs files..."
 cd protocol/
 
-docs_ovm_array=($(grep -r -l -i --include="*.sol" "solidity >=0.7.6" packages/core/contracts-ovm))
-mkdir ovm-docs/
-for doc in "${docs_ovm_array[@]}"
-  do
-    cp $doc ovm-docs/$doc
-  done
+grep -r -l -i --include="*.sol" "solidity >=0.7.6" packages/core/contracts-ovm > ovm_docs
 
-solidity-docgen --solc-module solc-0.8 -i packages/core/contracts/ -t ../ci/ -o temp-docs/
-solidity-docgen --solc-module solc-0.7 -i ovm-docs/ -t ../ci/ -o temp-docs/
+#docs_ovm_array=()
+sudo apt-get install libc++-dev
+mapfile -t docs_ovm_array < ovm_docs
+echo "docs:"
+echo $docs_ovm_array
 
-echo "configuring docs..."
-mv temp-docs/ ../docs/contracts
+#
+# for doc in "${docs_ovm_array[@]}"
+#   do
+#     cp $doc ovm-docs/$doc
+#   done
+#
+# solidity-docgen --solc-module solc-0.8 -i packages/core/contracts/ -t ../ci/ -o temp-docs/
+# solidity-docgen --solc-module solc-0.7 -i ovm-docs/ -t ../ci/ -o temp-docs/
+#
+# echo "configuring docs..."
+# mv temp-docs/ ../docs/contracts
