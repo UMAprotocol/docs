35f792967f3140cb01234787a76d3f62c87c8631
evaldofelipe <contato@evaldofelipe.com>
2021-08-31 23:27:12 -0300

    Change doc socl07 logic
    Signed-off-by: evaldofelipe <contato@evaldofelipe.com>

--- ci/solidity-docs.sh
+++ ci/solidity-docs.sh
@@ -3,35 +3,30 @@
 set -o errexit
 set -o nounset
 
+echo "installing required tools..."
+yarn install
+npm install -g solidity-docgen
 
-#
-# echo "installing required tools..."
-# yarn install
-# npm install -g solidity-docgen
-#
-# echo "installing protocol repo..."
+echo "installing protocol repo..."
 git clone http://github.com/umaprotocol/protocol/ --single-branch || echo "Repo already exists"
 yarn install --cwd protocol/packages/core --ignore-scripts
 
 echo "generating docs files..."
+filename=solc07_docs
 cd protocol/
 
-grep -r -l -i --include="*.sol" "solidity >=0.7.6" packages/core/contracts-ovm > ovm_docs
+grep -r -l -i --include="*.sol" "solidity >=0.7.6" packages/core/contracts-ovm > ${filename}
 
-#docs_ovm_array=()
-sudo apt-get install libc++-dev
-mapfile -t docs_ovm_array < ovm_docs
-echo "docs:"
-echo $docs_ovm_array
+mkdir ovm_docs
+docs_ovm_array=(`cat "$filename"`)
 
-#
-# for doc in "${docs_ovm_array[@]}"
-#   do
-#     cp $doc ovm-docs/$doc
-#   done
-#
-# solidity-docgen --solc-module solc-0.8 -i packages/core/contracts/ -t ../ci/ -o temp-docs/
-# solidity-docgen --solc-module solc-0.7 -i ovm-docs/ -t ../ci/ -o temp-docs/
-#
-# echo "configuring docs..."
-# mv temp-docs/ ../docs/contracts
+ for doc in "${docs_ovm_array[@]}"
+  do
+    cp ${doc} ovm_docs/
+  done
+
+solidity-docgen --solc-module solc-0.8 -i packages/core/contracts/ -t ../ci/ -o temp-docs/
+solidity-docgen --solc-module solc-0.7 -i ovm-docs/ -t ../ci/ -o temp-docs/
+
+echo "configuring docs..."
+mv temp-docs/ ../docs/contracts
