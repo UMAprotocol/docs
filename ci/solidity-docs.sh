b8ce5940bf3bd80d72ef2c5c6c8816cebd53d057
evaldofelipe <contato@evaldofelipe.com>
2021-09-01 17:31:52 -0300

    Add ovm 0.8 contracts
    Signed-off-by: evaldofelipe <contato@evaldofelipe.com>

--- ci/solidity-docs.sh
+++ ci/solidity-docs.sh
@@ -12,23 +12,42 @@ git clone http://github.com/umaprotocol/protocol/ --single-branch || echo "Repo 
 yarn install --cwd protocol/packages/core --ignore-scripts
 
 echo "generating docs files..."
-filename=solc07_docs
+
+solc07="solc07_docs"
+solc08="solc08_docs"
+solc07_folder="ovm_docs_07/"
+solc08_folder="ovm_docs_08/"
+output_docs_folder="temp-docs/"
+template_folder="../ci/"
+genreal_contracts_folder="packages/core/contracts/"
+
 cd protocol/
 
-grep -r -l -i --include="*.sol" "solidity >=0.7.6" packages/core/contracts-ovm > ${filename}
+grep -r -l -i --include="*.sol" "solidity >=0.7.6" packages/core/contracts-ovm > ${solc07}
+grep -r -l -i --include="*.sol" "solidity ^0.8.0" packages/core/contracts-ovm > ${solc08}
 
-mkdir ovm_docs || echo "Folder already exists"
-docs_ovm_array=(`cat "$filename"`)
+mkdir ${solc07_folder} ${solc08_folder} || echo "Folder already exists"
 
+contract_ovm_array=(`cat "$solc08"`)
  for doc in "${docs_ovm_array[@]}"
   do
-    cp ${doc} ovm_docs/
+    cp ${doc} ${solc08_folder}
   done
 
-echo "generating docs for contrats using solc-0.8"
-solidity-docgen --solc-module solc-0.8 -i packages/core/contracts/ -t ../ci/ -o temp-docs/
-echo "generating docs for contrats using solc-0.7"
-solidity-docgen --solc-module solc-0.7 -i ovm-docs/ -t ../ci/ -o temp-docs/
+contract_ovm_array=(`cat "$solc07"`)
+ for contract in "${contract_ovm_array[@]}"
+  do
+    cp ${contract} ${solc07_folder}
+  done
+
+echo "generating docs for general contrats using solc-0.8"
+solidity-docgen --solc-module solc-0.8 -i ${general_contracts_folder} -t ${template_folder} -o ${output_docs}
+
+echo "generating docs for ovm contrats using solc-0.8"
+solidity-docgen --solc-module solc-0.8 -i ${solc08_folder} -t ${template_folder} -o ${output_docs}
+
+echo "generating docs for ovm contrats using solc-0.7"
+solidity-docgen --solc-module solc-0.7 -i ${solc07_folder} -t ${template_folder} -o ${output_docs}
 
 echo "configuring docs..."
 mv temp-docs/ ../docs/contracts
